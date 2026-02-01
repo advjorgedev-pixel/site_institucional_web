826194408465bb30a941693405481301d4efb397
RICARDO LUCAS TIEPPO MARTINS <105604399+RTieppo@users.noreply.github.com>
2026-02-01 13:45:01 -0300

    lista de blog finalizada

--- institucional/views.py
+++ institucional/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import TemplateView, ListView, DetailView
 from django.utils import timezone
 from django.shortcuts import get_object_or_404
-from .models import BlogPost
+from .models import BlogPost, AreaOfPractice
 
 
 class HomeView(TemplateView):
@@ -17,19 +17,20 @@ class BlogPostListView(ListView):
     model = BlogPost
     template_name = "blog/blog_list.html"
     context_object_name = "posts"
-    paginate_by = 6
+    paginate_by = 1
 
     def get_queryset(self):
         qs = (
             BlogPost.objects
             .filter(status=BlogPost.Status.PUBLISHED, published_at__lte=timezone.now())
             .select_related("area", "created_by")
-            .prefetch_related("tags")
             .order_by("-published_at", "-created_at")
         )
+        
 
         q = (self.request.GET.get("q") or "").strip()
         area_slug = (self.request.GET.get("area") or "").strip()
+        print(f"Area slug: {area_slug}")
 
         if area_slug:
             qs = qs.filter(area__slug=area_slug)
@@ -44,9 +45,11 @@ class BlogPostListView(ListView):
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
         ctx["q"] = (self.request.GET.get("q") or "").strip()
+        ctx["areas"] = AreaOfPractice.objects.all()
         ctx["area_selected"] = (self.request.GET.get("area") or "").strip()
+        qs = self.get_queryset()
+        ctx["featured_post"] = qs.first()
         return ctx
-
 
 class ContactView(TemplateView):
     template_name = "contact/contact_site.html"
